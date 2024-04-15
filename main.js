6e72a742c03388667c708f867eff3cb6a8cce137
Gustavo <106815244+GustavoDCruz@users.noreply.github.com>
2024-04-15 10:34:27 -0300

    a

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
+function verificarTime() {
 
-function verificarTime(){
+    var continuarPerguntando = true;
 
-var continuarPerguntando = true;
+    while (continuarPerguntando) (
 
-while (continuarPerguntando)
+    var respostaTime = prompt("Qual o melhor time?");
 
+    if (respostaTime.toLowerCase() == "athlético pr") {
 
+    alert("Isso mesmo! 0 Athlético PR é o melhor time!");
 
+    continuarPerguntando = false; // Sai do loop após a respost
 
+    } else {
 
+    alert("Não é isso! Tente novamente.");
 
+    }
 
-
-
+// Chamada da função para iniciar o processo
+ verificarTime();
+}
